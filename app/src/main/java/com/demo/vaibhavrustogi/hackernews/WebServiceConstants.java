9e8a99c4a08eb88114b148951f484dd4f24771ff
Vaibhav Rustogi <vaibhav.rustogi@paytm.com>
2017-07-14 19:42:34 +0530

    fetch data from api on news detail

--- app/src/main/java/com/demo/vaibhavrustogi/hackernews/WebServiceConstants.java
+++ app/src/main/java/com/demo/vaibhavrustogi/hackernews/WebServiceConstants.java
@@ -10,12 +10,21 @@ public class WebServiceConstants {
 
     public static final String HACKER_NEWS_BASE_URL = "https://hacker-news.firebaseio.com/v0";
     public static final String TOP_STORIES = "topstories.json";
+    public static final String ITEM = "item";
+    public static final String JSON = ".json";
 
     public static final String KEY_ITEM_ID = "item_id";
 
     public static String getTopStoriesUrl() {
         Uri.Builder uriBuilder = Uri.parse(HACKER_NEWS_BASE_URL).buildUpon();
         uriBuilder.appendPath(TOP_STORIES);
+        return uriBuilder.toString();
+    }
+
+    public static String getItemUrl(long itemId) {
+        Uri.Builder uriBuilder = Uri.parse(HACKER_NEWS_BASE_URL).buildUpon();
+        uriBuilder.appendPath(ITEM);
+        uriBuilder.appendPath("" + itemId + JSON);
         return uriBuilder.toString();
     }
 }
--- app/src/main/java/com/demo/vaibhavrustogi/hackernews/fragments/NewsDetailFragment.java
+++ app/src/main/java/com/demo/vaibhavrustogi/hackernews/fragments/NewsDetailFragment.java
@@ -1,14 +1,56 @@
 package com.demo.vaibhavrustogi.hackernews.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.demo.vaibhavrustogi.hackernews.R;
+import com.demo.vaibhavrustogi.hackernews.WebServiceConstants;
+import com.demo.vaibhavrustogi.hackernews.model.Item;
+import com.demo.vaibhavrustogi.hackernews.networking.NetworkManager;
 
 /**
  * Created by vaibhavrustogi on 14/07/17.
  */
 
-public class NewsDetailFragment extends BaseFragment {
+public class NewsDetailFragment extends BaseFragment implements Response.Listener, Response.ErrorListener {
+
+    private long itemId;
+
     @Override
     protected int getFragmentLayout() {
         return R.layout.news_detail_layout;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        initializeViews();
+        fetchItemDetail(itemId);
+    }
+
+    private void fetchItemDetail(long itemId) {
+        NetworkManager.getsInstance(getActivity()).makeGsonRequestGet(WebServiceConstants.getItemUrl(itemId), Item.class, null, null, this, this);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            itemId = getArguments().getLong(WebServiceConstants.KEY_ITEM_ID);
+        }
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+
+    @Override
+    public void onResponse(Object response) {
+        if (response != null && response instanceof Item) {
+        }
     }
 }
