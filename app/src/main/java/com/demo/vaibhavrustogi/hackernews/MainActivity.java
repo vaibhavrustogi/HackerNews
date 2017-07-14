9b619522e625fefdb72110efb5c45af317800107
Vaibhav Rustogi <vaibhav.rustogi@paytm.com>
2017-07-14 17:43:51 +0530

    Created Repository with No Implementation

---
+++ app/src/main/java/com/demo/vaibhavrustogi/hackernews/MainActivity.java
@@ -0,0 +1,13 @@
+package com.demo.vaibhavrustogi.hackernews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
